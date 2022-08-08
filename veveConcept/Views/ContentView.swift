8da1cd3ca9b35e159c68e00647059097283149bc
Lior Dorenboust <liordorenboust@gmail.com>
2022-08-08 15:32:26 +0300

    Showing obBoarding screen after the Splash screen finished

--- veveConcept/Views/ContentView.swift
+++ veveConcept/Views/ContentView.swift
@@ -11,25 +11,34 @@ struct ContentView: View {
     
     @State var animationCircle: Bool = false
     @State var animationVeve: Bool = false
+    @State var showOnBoarding: Bool = false
     
     var body: some View {
+        //show onBoarding when splashScreen Finished
+        if showOnBoarding {
+            OnBoarding()
+        }
+        
         ZStack {
+            //veveBlue color Circle that increase  3 sec after view onAppear
             Circle()
                 .fill(Color("veveBlue"))
             
                 .frame(width: self.animationCircle ? 10000 : 100)
                 .frame(height: self.animationCircle ? 10000 : 100)
-                .animation(Animation.linear(duration: 3))
+//                .animation(Animation.linear(duration: 3))
+                
             
             VStack {
                 ZStack {
+                    
+                    //veve logo increase 2 sec after veveBlue color Circle finished
                     Image("logo")
                         .resizable()
                         .scaledToFit()
                         .frame(width: self.animationVeve ? 10000: 150)
                         .frame(height: self.animationVeve ? 10000: 150)
                         .offset(y: self.animationVeve ? -1400: 0)
-                    .animation(Animation.linear(duration: 0.7))
                     
                     Text("Premium Digital \n Collectibles")
                         .foregroundColor(.white)
@@ -37,10 +46,8 @@ struct ContentView: View {
                         .bold()
                         .offset(y: 125)
                         .multilineTextAlignment(.center)
-                        
-                }
                     
-                
+                }
                 
                 ProgressView()
                     .opacity(animationCircle ? 0 : 1)
@@ -48,15 +55,26 @@ struct ContentView: View {
             }
             
         }.onAppear{
+            
+            //controling the animation times
             DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                self.animationCircle.toggle()
+                withAnimation(.linear(duration: 3)) {
+                    self.animationCircle.toggle()
+                }
                 
+
                 DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    self.animationVeve.toggle()
+                    withAnimation(.linear(duration: 0.7)) {
+                        self.animationVeve.toggle()
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                            self.showOnBoarding.toggle()
+                        }
+                    }
+
                 }
             }
         }
-        
     }
 }
 
@@ -65,3 +83,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ veveConcept/Views/OnBoarding.swift
@@ -0,0 +1,20 @@
+//
+//  OnBoarding.swift
+//  veveConcept
+//
+//  Created by Lior Dorenboust on 08/08/2022.
+//
+
+import SwiftUI
+
+struct OnBoarding: View {
+    var body: some View {
+        Text("ImBoarding")
+    }
+}
+
+struct OnBoarding_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoarding()
+    }
+}
